a4dda43990d494ad1a0b4d0eb1400196f6a4c131
v1xingyue <qixingyue@gmail.com>
2025-02-05 15:21:43 +0800

    displayTweet

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
 import { getScraper, logger, saveCookies } from "./utils";
 import { db } from "./db";
+import { Tweet } from "agent-twitter-client";
+
+const displayTweet = (tweet: Tweet) => {
+  logger.info(`\n ===== ${tweet.id} ===== \n`);
+  logger.info(tweet.text);
+  logger.info(`photos: ${JSON.stringify(tweet.photos)}`);
+  logger.info(`videos: ${JSON.stringify(tweet.videos)}`);
+  logger.info(`urls: ${JSON.stringify(tweet.urls)}`);
+  logger.info(`mentions: ${JSON.stringify(tweet.mentions)}`);
+  logger.info(`hashtags: ${JSON.stringify(tweet.hashtags)}`);
+  logger.info(`timestamp: ${tweet.timestamp}`);
+};
 
 const main = async () => {
   const scraper = await getScraper();
   await saveCookies(scraper);
   const tweets = scraper.getTweets("ai16zdao", 20);
   for await (const tweet of tweets) {
-    logger.info(`\n ===== ${tweet.id} ===== \n`);
-    logger.info(tweet.text);
-    logger.info(`photos: ${JSON.stringify(tweet.photos)}`);
-    logger.info(`videos: ${JSON.stringify(tweet.videos)}`);
-    logger.info(`urls: ${JSON.stringify(tweet.urls)}`);
-    logger.info(`mentions: ${JSON.stringify(tweet.mentions)}`);
-    logger.info(`hashtags: ${JSON.stringify(tweet.hashtags)}`);
-    logger.info(`timestamp: ${tweet.timestamp}`);
-
+    displayTweet(tweet);
     if (tweet.text && tweet.id) {
       const is_done = await db.get(tweet.id.toString());
       logger.info(`is_done: ${is_done}`);
